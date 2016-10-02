a8f7d35a8e47831374a60ce2c55c8624a4391891
Pidstrela <monk12@i.ua>
2016-10-03 01:18:31 +0200

    New Commit

--- server/controllers/authentication-controller.js
+++ server/controllers/authentication-controller.js
@@ -3,7 +3,7 @@ var db = require('.../server.js');
 
 module.exports.signup = function (req, res){
 
-/**
+
 		Users.create(req.body, function (err, user) {
 		if (err) throw err;
 		console.log('User created!');
@@ -13,21 +13,8 @@ module.exports.signup = function (req, res){
 		});
 		res.end('Added the user with id: ' + id);
 	});
-**/
 
-
-		  db.collection(Users).insertOne(req.body, function(err, doc) {
-		if (err) throw err;
-		console.log('User created!');
-		var id = user._id;
-		res.writeHead(200, {
-			'Content-Type': 'text/plain'
-		});
-		res.end('Added the user with id: ' + id);
-
-	}
-
-	)};
+	};
 
 module.exports.login = function (req, res){
 	Users.find(req.body, function (err, results){
