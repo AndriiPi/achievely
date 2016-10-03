6efad0bb06095657dea2a312002a32a86d6a9d56
Pidstrela <monk12@i.ua>
2016-10-03 19:54:46 +0200

    b

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ angular.module('dovalley', ['ui.router'])
 						controller: 'HeaderController'
 					},
 					 'content': {
-						 templateUrl: 'public/views/dashboard.html',
-						 controller:  'DashboardController'
+						 templateUrl: 'public/views/landing.html',
+						 controller: 'LandingController'
 					 },
 					'footer': {
 						templateUrl: 'public/views/footer.html'
